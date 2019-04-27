07900e30019164bcec1574cc2d0c9a7039d5acb6
Jamalzade Elvin Rasim  <Jama_yw17@itstep.edu.az>
2019-04-27 21:08:38 +0400

    Add project files.

---
+++ SchoolSystem/ModelView/StudentViewModel.cs
@@ -0,0 +1,25 @@
+﻿using SchoolSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.ModelView
+{
+    class StudentViewModel : BaseViewModel
+    {
+        private List<Student> allstudent;
+
+        public List<Student> AllStudent
+        {
+            get { return allstudent; }
+            set
+            {
+                allstudent = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("AllStudents"));
+            }
+        }
+    }
+}
